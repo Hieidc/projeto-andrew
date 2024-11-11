df857daa1ea08c8d7f8dfa2cbb994aa088137032
Andrew Kerscher <andrew.kerscher@outlook.com>
2024-11-11 16:00:23 -0300

    autogui

---
+++ appautogui.py
@@ -0,0 +1,11 @@
+import pyautogui
+from time import sleep
+
+#Posicao algo - use o mouseInfo pelo cmd.
+#Fazer algo com a posicao
+
+pyautogui.moveTo(729,482,duration=2)
+#pyautogui.move(50,0,duration=2)
+for i in range(62):
+    sleep(0.5)
+    pyautogui.click()
